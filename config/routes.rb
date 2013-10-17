1523f0e832987f0b25660208522d94c895b6088a
Gaurav Saini <saini_g@hcl.com>
2013-10-17 18:47:37 +0530

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Knowledgeofrails::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'posts#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -55,4 +55,6 @@ Knowledgeofrails::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  #  match ':controller(/:action(/:id(.:format)))'
+
 end
