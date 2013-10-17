8b5a34b0d24a31f4c02da913418041d9c4a30d83
Gaurav Saini <saini_g@hcl.com>
2013-10-17 16:13:07 +0530

    start app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Knowledgeofrails::Application.config.secret_key_base = '65e0170a0fd665efe784ae3842cdd31215ae8409c5c017fb17071e918f7338e5740d5b262455ee0f40dc3d3a1d1a8275aaa8c6c4d879b7674d85d371293a183b'
